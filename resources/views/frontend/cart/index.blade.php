f20bfae6f611a95eb0054aee717a8afeff120008
Prince Louis M. Jaylo <jayloprincelouis123@gmail.com>
2025-03-15 17:32:52 +0800

    cart

--- resources/views/frontend/cart/index.blade.php
+++ resources/views/frontend/cart/index.blade.php
@@ -20,47 +20,53 @@
                 <div class="col-lg-12">
                     <form action="{{ route('cart.update') }}" method="post">
                         @csrf
-                    <div class="shop__cart__table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Product</th>
-                                    <th>Price</th>
-                                    <th>Quantity</th>
-                                    <th>Total</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($data['carts'] as $carts)
+                        <div class="shop__cart__table">
+                            <table>
+                                <thead>
                                     <tr>
-                                        <td class="cart__product__item">
-                                            <img src="{{ asset($carts->Product->thumbnails_path) }}" alt="" width="90">
-                                            <div class="cart__product__item__title">
-                                                <h6>{{ $carts->Product->name }}</h6>
-                                                <div class="rating">
-                                                    <i class="fa fa-star"></i>
-                                                    <i class="fa fa-star"></i>
-                                                    <i class="fa fa-star"></i>
-                                                    <i class="fa fa-star"></i>
-                                                    <i class="fa fa-star"></i>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">{{ $carts->Product->price_rupiah }}</td>
-                                        <input type="hidden" name="cart_id[]" value="{{ $carts->id }}">
-                                        <td class="cart__quantity">
-                                            <div class="pro-qty">
-                                                <input type="text" value="{{ $carts->qty }}" name="cart_qty[]">
-                                            </div>
-                                        </td>
-                                        <td class="cart__total">{{ rupiah($carts->total_price_per_product) }}</td>
-                                        <td class="cart__close"><a href="{{ route('cart.delete',$carts->id) }}"><span class="icon_close"></span></a></td>
+                                        <th>Product</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Total</th>
+                                        <th></th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                                </thead>
+                                <tbody>
+                                    @foreach ($data['carts'] as $carts)
+                                        <tr>
+                                            <td class="cart__product__item">
+                                                <img src="{{ asset($carts->Product->thumbnails_path ?? 'default.jpg') }}" alt="" width="90">
+                                                <div class="cart__product__item__title">
+                                                    <h6>{{ $carts->Product->name ?? 'Product Unavailable' }}</h6>
+                                                    <div class="rating">
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="cart__price">
+                                                {{ formatPeso($carts->Product->price ?? 0) }}
+                                            </td>
+                                            <input type="hidden" name="cart_id[]" value="{{ $carts->id }}">
+                                            <td class="cart__quantity">
+                                                <div class="pro-qty">
+                                                    <input type="text" value="{{ $carts->qty }}" name="cart_qty[]">
+                                                </div>
+                                            </td>
+                                            <td class="cart__total">
+                                                {{ formatPeso($carts->total_price_per_product) }}
+                                            </td>
+                                            <td class="cart__close">
+                                                <a href="{{ route('cart.delete', $carts->id) }}"><span class="icon_close"></span></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                 </div>
             </div>
             <div class="row">
@@ -72,18 +78,17 @@
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="cart__btn update__btn">
                         <button type="submit"><span class="icon_loading"></span> Update cart</button>
-                    </form>
                     </div>
                 </div>
             </div>
+            </form> <!-- Ensure form is properly closed here -->
             <div class="row">
-                <div class="col-lg-6">
-                </div>
+                <div class="col-lg-6"></div>
                 <div class="col-lg-4 offset-lg-2">
                     <div class="cart__total__procced">
                         <h6>Cart total</h6>
                         <ul>
-                            <li>Total <span>{{ rupiah($data['carts']->sum('total_price_per_product')) }}</span></li>
+                            <li>Total <span>{{ formatPeso($data['carts']->sum('total_price_per_product')) }}</span></li>
                         </ul>
                         <a href="{{ route('checkout.index') }}" class="primary-btn">Proceed to checkout</a>
                     </div>
